cb3c591483964f489853044c5a8654ab966531fe
Elena Podshybiakina <puchochek@gmail.com>
2019-06-27 15:17:42 +0300

    Modal form styled to material design

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 //import {MatCardModule, MatButtonModule} from '@angular/material/card';
 import { MatCardModule, MatButtonModule } from '@angular/material';
 
+
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
