87b00bd6bdd58760466d2cf59ebbbbd68bf17af4
Elena Podshybiakina <puchochek@gmail.com>
2019-07-01 10:33:27 +0300

    Profile menu style changed

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -12,20 +12,22 @@ export class UserProfileComponent implements OnInit {
 	menuOpenedIcon: string;
 	menuClosedIcon: string;
 	menuIconToDisplay: string;
-	menuOptions = [
-		'profile settings',
-		'manage categories',
-		'create report'
-	];
+	menuOptions = [];
 
 	constructor() { }
 
 	ngOnInit() {
+		const initialMenuOptions = [
+			{ value: 'profile settings', isActive: true },
+			{ value: 'manage categories', isActive: false },
+			{ value: 'create report', isActive: false }
+		];
+		this.menuOptions = initialMenuOptions;
 		this.menuClosedIcon = `arrow_back_ios`;
 		this.menuIconToDisplay = this.menuClosedIcon;
 	}
 
-	onOpenedChange(e: boolean) {		
+	onOpenedChange(e: boolean) {
 		if (this.menuClosedIcon) {
 			this.menuClosedIcon = undefined;
 			this.menuOpenedIcon = `arrow_forward_ios`;
@@ -38,10 +40,24 @@ export class UserProfileComponent implements OnInit {
 		}
 	}
 
+	onMenuItemClicked(event) {
+		const target = event.target || event.srcElement || event.currentTarget;
+		const idAttr = target.attributes.id;
+		const selectedOptionId = idAttr.nodeValue;		
+		const switchedMenuOptions = this.menuOptions.reduce((switchedMenuOptions, menuOption, currentIndex, array) => {			
+			const isActive = currentIndex === Number(selectedOptionId) ?
+				true
+				: false;
+			switchedMenuOptions.push({value: menuOption.value, isActive: isActive});
+			return switchedMenuOptions;
+		}, []);		
+		this.menuOptions = switchedMenuOptions;
+	}
+
 
 	//TODO cant track exact target from here
 	// @HostListener('click') onClick() {		
-		
+
 	// 	if (this.menuClosedIcon) {
 	// 		this.menuClosedIcon = undefined;
 	// 		this.menuOpenedIcon = `arrow_back_ios`;
