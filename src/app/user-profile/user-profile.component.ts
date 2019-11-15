15393af80a7587c38a691b2b8b83789805808374
Elena <puchochek@gmail.com>
2019-11-15 17:48:30 +0300

    User-profile avatar started

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -14,68 +14,64 @@ import { environment } from '../../environments/environment'
 export class UserProfileComponent implements OnInit {
 
 	currentUser: LoggedUser;
-	settingsToolTip: string;
-	categoriesToolTip: string;
-	settingsTabLabel: string;
-	categoriesTabLabel: string;
+	avatarLabel = `Avatar`;
+	themeLabel = `App theme`;
+	userInfoLabel = `User Info`;
+	currencyLabel = `Currency`;
+	balanceLimitLabel = `Balance limit`;
+
+	bgColor = "#8e8e8e";
+	color = "white";
+
+	avatarSrc: string;
+	avatarInitials: string;
 
 	constructor(
 		private http: HttpClient,
 		private dataService: DataService,
 		private router: Router,
-	) {
-		// this.dataService.loggedUser.subscribe((data) => {
-		// 	console.log('--->  USER FROM SERVICE loggedUser ', data);
-
-		// }
-		// );
-
-	}
+	) { }
 
 	ngOnInit() {
-		this.settingsTabLabel = `Settings`;
-		this.categoriesTabLabel = `Categories`;
-		this.settingsToolTip = `Define your Profile settings here`;
-		this.categoriesToolTip = `Set end manage your transactions categories here`;
-		this.dataService.loggedUser.subscribe((response) => {
-			console.log('--->  USER FROM SERVICE loggedUser INIT', response);
-			if (response) {
-				this.currentUser = <LoggedUser>response;
-			} else {
-				this.router.navigate(['/hello-monney']);
+		const userId = localStorage.getItem("userId");
+		const url = `${environment.apiBaseUrl}/user/user-by-id/${userId}`;
+		if (userId) {
+			this.http.get(url, { observe: 'response' })
+				.subscribe(
+					response => {
+						this.currentUser = <LoggedUser>response.body;
+						console.log('---> USER PROFILE response ', response);
+						this.setCurrentAvatar();
+						this.dataService.updateToken(response.headers.get('Authorization'));
+					},
+					error => {
+						console.log('---> USER PROFILE error ', error);
+						this.router.navigate(['/hello-monney']);
+					},
+					() => {
+						// 'onCompleted' callback.
+						// No errors, route to new page here
+					}
+				);
+		}
+	}
+
+	setCurrentAvatar() {
+		if (this.currentUser.avatar) {
+			this.avatarSrc = this.currentUser.avatar;
+		} else {
+			if (this.currentUser.name.split(` `)) {
+				this.avatarInitials = this.currentUser.name.split(` `).length > 1 ?
+					this.currentUser.name.split(` `)[0].slice(0, 1) + this.currentUser.name.split(` `)[1].slice(0, 1)
+					: this.currentUser.name.slice(0, 1);
 			}
-
-		});
-
-		// const userId = localStorage.getItem('userId');
-		// const url = `${environment.apiBaseUrl}/user/user-by-id/${userId}`;
-
-		// this.http.get(url, { observe: 'response' })
-		// 	.subscribe(
-		// 		response => {
-		// 			this.currentUser = <LoggedUser>response.body;
-		// 			console.log('---> UserProfileComponent response ', response);
-		// 			console.log('---> UserProfileComponent resp.headers.authorization ', response.headers.get('Authorization'));
-		// 			this.dataService.updateToken(response.headers.get('Authorization'));
-		// 		},
-		// 		error => {
-		// 			console.log('---> UserProfileComponent error ', error);
-		// 			//this.dataService.cleanLocalstorage();
-		// 			this.router.navigate(['/hello-monney']);
-
-		// 			//   this.errors = error;
-		// 		},
-		// 		() => {
-		// 			// 'onCompleted' callback.
-		// 			// No errors, route to new page here
-		// 		}
-		// 	);
-
+		}
 	}
 
-	logout() {
-		this.router.navigate(['/hello-monney']);
-		this.dataService.cleanLocalstorage();
+	onFileChanged(event) {
+		const file = event.target.files[0];
+		console.log('---> file ', file );
 	}
+
 
 }
