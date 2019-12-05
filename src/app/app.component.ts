ad9b1e4c458e4d49ccb8f89971b89c6f34c7bd2f
Elena <puchochek@gmail.com>
2019-12-05 09:04:46 +0300

    Select-background option is temporary removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,70 +16,14 @@ import { ThemeService } from './theme.service';
 })
 export class AppComponent {
 	TITLE = `monney`;
-	DEFAULT_THEME = `url(../assets/images/dark-theme.jpg)`;
-	currentUser: LoggedUser;
-	private sbscr: Subscription;
 
 	constructor(
-		private dataService: DataService,
-		private http: HttpClient,
-		public userService: UserService,
-		private router: Router,
-		private themeService: ThemeService,
+		// private dataService: DataService,
+		// private http: HttpClient,
+		// public userService: UserService,
+		// private router: Router,
+		// private themeService: ThemeService,
 
 	) { }
 
-	ngOnInit() {
-		this.sbscr = this.userService._user.subscribe((response) => {
-			console.log('--->  MAIN userServise INIT', response);
-			if (response) {
-				this.currentUser = <LoggedUser>response;
-				this.setAppBackground();
-			} else {
-				this.doUserControllerCall();
-			}
-		});
-	}
-
-	ngOnDestroy() {
-		this.sbscr.unsubscribe();
-	}
-
-	doUserControllerCall() {
-		const token = localStorage.getItem("token");
-		if (token) {
-			const tokenisedId = localStorage.getItem("token").split(" ")[1];
-			const url = `${environment.apiBaseUrl}/user/user-by-token/${tokenisedId}`;
-			this.http.get(url, { observe: 'response' })
-				.subscribe(
-					response => {
-						console.log('--->  MAIN doUserControllerCall ', response);
-						this.currentUser = <LoggedUser>response.body;
-						this.userService.appUser = this.currentUser;
-						this.setAppBackground();
-					},
-					error => {
-						console.log('---> MAIN error ', error);
-						this.router.navigate(['/hello-monney']);
-					},
-					() => {
-						// 'onCompleted' callback.
-						// No errors, route to new page here
-					}
-				);
-		} else {
-			document.getElementById('monney-app').style.backgroundImage = this.themeService.DEFAULT_THEME;
-			this.router.navigate(['/hello-monney']);
-		}
-
-	}
-
-	setAppBackground() {
-		console.log('---> setAppBackground ', this.currentUser.theme);
-		if (this.themeService.checkIfThemeExist(this.currentUser.theme)) {
-			document.getElementById('monney-app').style.backgroundImage = `url(../assets/images/${this.currentUser.theme}.jpg)`;
-		} else {
-			document.getElementById('monney-app').style.backgroundImage = `url(../assets/images/${this.themeService.DEFAULT_THEME}.jpg)`;
-		}
-	}
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -40,21 +40,6 @@ export class UserProfileComponent implements OnInit {
 	color: string = "white";
 	avatarSrc: string;
 	avatarInitials: string;
-	// assetsList: string[] = [
-	// 	`../assets/images/dark-theme.jpg`,
-	// 	`../assets/images/blue-theme.jpg`,
-	// 	`../assets/images/castle-theme.jpg`,
-	// 	`../assets/images/grey-theme.jpg`,
-	// 	`../assets/images/leaf-theme.jpg`,
-	// 	`../assets/images/paper-theme.jpg`,
-	// 	`../assets/images/swing-theme.jpg`,
-	// 	`../assets/images/red-theme.jpg`,
-	// 	`../assets/images/blue-pattern.jpg`,
-	// 	`../assets/images/purple-theme.jpg`,
-	// 	`../assets/images/wooden-theme.jpg`,
-	// 	`../assets/images/space-theme.jpg`
-	// ];
-	assetsList: string[];
 
 	public uploader: FileUploader;
 
@@ -68,7 +53,6 @@ export class UserProfileComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-		this.assetsList = [...this.themeService.ASSETS_LIST];
 		const token = localStorage.getItem("token");
 		if (token) {
 			const tokenisedId = localStorage.getItem("token").split(" ")[1];
@@ -203,24 +187,6 @@ export class UserProfileComponent implements OnInit {
 			return false;
 		} else {
 			return true;
-		}
-	}
-
-	selectBackground(event) {
-		const selectedImgSrc = event.explicitOriginalTarget.currentSrc;
-		console.log('--selectedImgSrc ', selectedImgSrc);
-		if (selectedImgSrc) {
-			const lastSlashIndex = selectedImgSrc.lastIndexOf(`/`);
-			const pointIndex = selectedImgSrc.indexOf(`.`);
-			const selectedAssetName = selectedImgSrc.substring((lastSlashIndex + 1), pointIndex);
-			console.log('---> selectedAssetName ', selectedAssetName);
-			if (this.themeService.checkIfThemeExist(selectedAssetName)) {
-				const userToUpdate: LoggedUser = { ...this.currentUser };
-				userToUpdate.theme = selectedAssetName;
-				this.isLoading = true;
-				this.doUpdateUserCall([userToUpdate]);
-				this.userService.appUser = this.currentUser;
-			}
 		}
 	}
 
