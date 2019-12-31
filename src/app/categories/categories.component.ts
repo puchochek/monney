e69b0bca604138ba321ce733760a9fa04b96f93c
Elena <puchochek@gmail.com>
2019-12-31 13:31:55 +0300

    Category cmp, SelectIcon cmp added

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApplicationUser, Category } from '../interfaces';
 import { CategoryService } from '../category.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
 	selector: 'app-categories',
@@ -17,12 +18,17 @@ export class CategoriesComponent implements OnInit {
 
 	constructor(
 		private categoryService: CategoryService,
+		private router: Router,
 	) { }
 
 	ngOnInit() {
 		console.log('---> currentUser ', this.currentUser);
 		this.categories = this.categoryService.getExpencesCategories(this.currentUser.categories);
 		this.noCategoriesMessage = `Hello, ${this.currentUser.name}. You don't have an expences categories yet. It would be great to add some to keep your expences in order.`;
+	}
+
+	addCategory() {
+		this.router.navigate(['/category/add']);
 	}
 
 }
